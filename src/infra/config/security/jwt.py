13a67b49f992a829d4615f81e5ff3764960e945c
Jamerson Carlos Dos Santos <64469941+JamersonCarlos@users.noreply.github.com>
2025-07-24 00:23:08 -0300

    atualizando método de criação do token

--- src/infra/config/security/jwt.py
+++ src/infra/config/security/jwt.py
@@ -23,13 +23,13 @@ credentials_exception = HTTPException(
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES))
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
 def verify_token(token: str):
-    try:
+    try:    
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         return payload
     except JWTError:
